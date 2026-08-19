ce540c768d4cc47a62dee4a33a190856c77c248f
Gustavo <gustavu.dev@gmail.com>
2026-08-19 20:48:39 -0300

    Update Beau Clinic landing page

---
+++ src/lib/beau.ts
@@ -0,0 +1,5 @@
+export const TRINKS_URL = "https://www.trinks.com/a-beau-clinic-";
+export const INSTAGRAM_URL = "https://instagram.com/beauclinc";
+export const WHATSAPP_URL = "https://wa.me/5571997230824";
+export const MAPS_URL =
+  "https://www.google.com/maps/search/?api=1&query=Rua+Marcelo+L.+Pereira,+50,+Alagoinhas+-+BA";
